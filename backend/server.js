9004e5322f29bd0ef1b7ffc1d1d2a1eed8a130ee
Ashwin <ashwinshenoy333@gmail.com>
2025-12-07 22:43:55 +0530

    undo auth changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-require('./models/Player');
 
 const app = express();
 
@@ -12,7 +11,6 @@ app.use(express.json());
 
 // Routes
 app.use('/api/players', require('./routes/players'));
-app.use('/api/auth', require('./routes/auth'));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
