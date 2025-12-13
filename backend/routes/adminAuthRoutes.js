eed6559d1247149067b369360e828fa94be358f3
Ashwin <ashwinshenoy333@gmail.com>
2025-12-13 23:17:19 +0530

    adminlogin3

--- backend/routes/adminAuthRoutes.js
+++ backend/routes/adminAuthRoutes.js
@@ -33,11 +33,11 @@ router.post("/login", async (req, res) => {
     );
 
     res.cookie("adminToken", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
-      maxAge: 24 * 60 * 60 * 1000
-    });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  maxAge: 24 * 60 * 60 * 1000
+});
 
     res.status(200).json({
       message: "Admin login successful"
